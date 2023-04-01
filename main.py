6b975670abdec85dacdad4bf12de58bd953f3e2d
yasasvi <yasasvigumma@gmail.com>
2023-04-01 22:28:39 +0530

    step-2 of build

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from newspaper import Article
+from textblob import TextBlob
+import nltk
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return 'Hello'
+    return render_template("index.html")
+
+@app.route("/result",methods = ['POST',"GET"])
+def result():
+    if request.method=='POST':
+        url = request.form['url']
+        nltk.download('punkt')
+        artical = Article(url)
+        artical.download()
+        artical.parse()
+        artical.nlp()
+        text = artical.summary
+        blob = TextBlob(text)
+        sentiment = blob.sentiment.polarity
+        if sentiment >= 0:
+            return render_template("result.html",text = text, review = "Good")
+        else:
+            return render_template("result.html",text = text, review = "Bad")
+
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=True,port=5000)
